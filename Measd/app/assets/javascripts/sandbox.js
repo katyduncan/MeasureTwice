d9abc4b58e508cd87b4d3ee176f0db931ce9491d
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2015-06-23 18:54:15 -0700

    push up work from other branch

--- Measd/app/assets/javascripts/sandbox.js
+++ Measd/app/assets/javascripts/sandbox.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-  var draw = SVG('drawing').size(800, 600)
+  var draw = SVG('drawing').size(800, 600);
+
+  var drawOffSetX = draw.offsetLeft;
+  var drawOffSetY = draw.offsetTop;
 
   var roomWidth = 550;
   var roomLength = 350;
@@ -107,5 +110,111 @@ $(document).ready(function(){
       element.draggable()
 
     })
+
+    var selected = false;
+    var a = -1;
+    var selectTop, selectBottom, selectLeft, selectRight;
+    selectTop = draw.line(0,0,0,0).stroke({width: 1}); selectBottom = draw.line(0,0,0,0).stroke({width: 1}); selectLeft = draw.line(0,0,0,0).stroke({width: 1}); selectRight = draw.line(0,0,0,0).stroke({width: 1});
+    var knob = draw.circle(0).stroke({color: "blue", width: 2});
+    var connectKnob = draw.circle(0);
+    var connectLine = draw.line(0,0,0,0);
+    connectKnob.attr({fill: "blue"});
+    knob.attr({ fill: "green"});
+    knob.attr('name','rotationKnob');
+    var padding=3;
+    var set1 = draw.set();
+    set1.add(selectTop); set1.add(selectBottom); set1.add(selectRight); set1.add(selectLeft);
+    set1.add(knob); set1.add(connectKnob); set1.add(connectLine);
+    var vmousedown = false;
+    var mouseX, mouseY;
+    var deltaX_old = 0;
+    var deltaY_old = 0;
+    var deltaX,deltaY;
+    var centerX,centerY;
+    var mOldX,mOldY;
+    var mPreviousAngle, mCurrentAngle;// both are angles relative to center of object
+    var deltaAngle;
+//end variable intialization
+    $('svg').on('click', function(){
+      if(a>-1){ a -= 1; }
+        //console.log("event -",a);
+       //console.log(this.getAttribute('id'));
+      // console.log(element.type);
+      if(a == -1 ){
+        selectTop.plot(0,0,0,0).stroke({ width: 1});
+        selectBottom.plot(0,0,0,0).stroke({ width: 1});
+        selectLeft.plot(0,0,0,0).stroke({ width: 1});
+        selectRight.plot(0,0,0,0).stroke({ width: 1});
+        knob.radius(0);
+        connectKnob.radius(0);
+        connectLine.plot(0,0,0,0).stroke({ width: 1});
+      }
+    })
+
+$('svg').on('mousedown','circle[name = "rotationKnob"]',function(ev){
+  vmousedown = true;
+  mouseX = ev.pageX;
+  mouseY = ev.pageY;
+  centerX = element.cx();
+  centerY = element.cy();
+  mOldX = ev.pageX - drawOffSetX - centerX;
+  mOldY = ev.pageY - drawOffSetY - centerY;
+});
+$('svg').on('mousemove',function(ev){
+   if (vmousedown === false){ return; }
+   //minus 90 puts it in perspective from object center
+   mPreviousAngle = 90 - Math.atan(mOldY/mOldX)*(360/(2*Math.PI));;
+   mCurrentAngle = 90 - Math.atan( (ev.pageY- drawOffSetY - centerY)  / (ev.pageX- drawOffSetX - centerX) )*(360/(2*Math.PI));
+   deltaAngle = mCurrentAngle - mPreviousAngle;
+
+   console.log(centerX, centerY);
+   console.log(mOldX, mOldY);
+   //console.log(mPreviousAngle,mCurrentAngle,deltaAngle);
+   degrees = deltaAngle*-1;
+   if( ((ev.pageY- drawOffSetY - centerY) > 0) && ((ev.pageX- drawOffSetX - centerX) < 0))
+   {
+      degrees = 180 + degrees;
+   }
+
+   console.log("degrees = "+ degrees);
+   degrees = element.transform("rotation") + degrees;
+
+   if(/*Math.abs(degrees)*/degrees > 360){
+     degrees = degrees % 360;
+   }
+   console.log(degrees);
+   element.rotate(degrees);
+   set1.rotate(degrees, centerX, centerY);
+   mOldX = ev.pageX - drawOffSetX - centerX;
+   mOldY = ev.pageY - drawOffSetY - centerY;
+});//end mousemove event
+
+$('svg').on('mouseup',function(ev){
+  if (vmousedown === false){ return; }
+
+   vmousedown = false;
+
+});//end mouseup event
+
+
+$('svg').on('click', 'g[name="sandbox"] image', function(){
+      if(a==-1){a +=2;}
+      if(a==0){a+=1}
+      element = SVG.get(this.getAttribute('id'))
+
+    if(a == 1 || 0 ){
+       var x = element.x() - padding;
+       var x2 = x + element.width() + padding;
+       var y = element.y() - padding;
+       var y2 = y + element.height() + padding;
+        selectTop.plot(x, y, x2, y ).stroke({ width: 1});
+        selectBottom.plot(x, y2, x2, y2 ).stroke({ width: 1});
+        selectLeft.plot(x, y, x, y2 ).stroke({ width: 1});
+        selectRight.plot(x2, y, x2, y2 ).stroke({ width: 1});
+        knob.radius(4); knob.move(x-2 +(x2-x)/2,y2+18)
+        connectKnob.radius(4); connectKnob.move(x-2 +(x2-x)/2,y2);
+        connectLine.plot(x+2 +(x2-x)/2,y2+4,x+2 +(x2-x)/2,y2+17).stroke({ width: 1});
+     }
+  })
 
   })
