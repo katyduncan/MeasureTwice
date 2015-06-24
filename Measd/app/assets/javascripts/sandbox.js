d1ff511ac6d50b52ba552620f1e702f7a6722a21
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2015-06-24 16:44:26 -0700

    trying to merge

--- Measd/app/assets/javascripts/sandbox.js
+++ Measd/app/assets/javascripts/sandbox.js
@@ -41,10 +41,6 @@ $(document).ready(function(){
   bed.move(40,410)
   bed.attr({name: 'Bed'})
 
-  //var rect = draw.rect.fill('images/shade.jpg')
-  //var test = rect.fill(draw.image('/assets/couch.png', 90, 40))
-  //rect.move(100,100)
-
   var desk = draw.image('/assets/desk.png', 60,30)
   desk.move(110,410)
   desk.attr({name: 'Desk', preserveAspectRatio: 'none'})
@@ -136,12 +132,11 @@ $(document).ready(function(){
     // element.draggable(false)
     // element.transform({ rotation: $('#furn_rotation').val() })
     // element.draggable()
-
   });
   // var svg_string = draw.svg();
   // submitFloorplan(svg_string);
   // viewFloorplan();
-});
+//}); katie found this ********************************************
 
 // var submitFloorplan = function(svgExport){
 //     $('#floorplan_button').on('click', function(e) {
@@ -263,74 +258,58 @@ $(document).ready(function(){
       }
     })
 
-$('svg').on('mousedown','circle[name = "rotationKnob"]',function(ev){
-  vmousedown = true;
-  mouseX = ev.pageX;
-  mouseY = ev.pageY;
-  centerX = element.cx();
-  centerY = element.cy();
-  mOldX = ev.pageX - drawOffSetX - centerX;
-  mOldY = ev.pageY - drawOffSetY - centerY;
-});
-$('svg').on('mousemove',function(ev){
-   if (vmousedown === false){ return; }
-   //minus 90 puts it in perspective from object center
-  mPreviousAngle = 90 - Math.atan(mOldY/mOldX)*(360/(2*Math.PI));
-  var mPreviousAngle2 = angleGetter(mOldX,mOldY,mPreviousAngle);
-  console.log("previous ",mPreviousAngle, mPreviousAngle2);
+  $('svg').on('mousedown','circle[name = "rotationKnob"]',function(ev){
+    vmousedown = true;
+    mouseX = ev.pageX;
+    mouseY = ev.pageY;
+    centerX = element.cx();
+    centerY = element.cy();
+    mOldX = ev.pageX - drawOffSetX - centerX;
+    mOldY = ev.pageY - drawOffSetY - centerY;
+  });
+
+  $('svg').on('mousemove',function(ev){
+    if (vmousedown === false){ return; }
+    //minus 90 puts it in perspective from object center
+    mPreviousAngle = 90 - Math.atan(mOldY/mOldX)*(360/(2*Math.PI));
+    var mPreviousAngle2 = angleGetter(mOldX,mOldY,mPreviousAngle);
+    console.log("previous ",mPreviousAngle, mPreviousAngle2);
 
 
-   mCurrentAngle = 90 - Math.atan( (ev.pageY- drawOffSetY - centerY)  / (ev.pageX- drawOffSetX - centerX) )*(360/(2*Math.PI));
-   var mCurrentAngle2 = angleGetter((ev.pageX- drawOffSetX - centerX),(ev.pageY- drawOffSetY - centerY),mCurrentAngle);
-  console.log("Current ",mCurrentAngle, mCurrentAngle2);
+    mCurrentAngle = 90 - Math.atan( (ev.pageY- drawOffSetY - centerY)  / (ev.pageX- drawOffSetX - centerX) )*(360/(2*Math.PI));
+    var mCurrentAngle2 = angleGetter((ev.pageX- drawOffSetX - centerX),(ev.pageY- drawOffSetY - centerY),mCurrentAngle);
+    console.log("Current ",mCurrentAngle, mCurrentAngle2);
+    deltaAngle = mCurrentAngle2 - mPreviousAngle2;
+    degrees = deltaAngle*-1;
+    degrees = element.transform("rotation") + degrees;
 
-  deltaAngle = mCurrentAngle2 - mPreviousAngle2;
+    if(degrees > 360){
+      degrees = degrees % 360;
+    }
 
-   //console.log(centerX, centerY);
-   //console.log(mOldX, mOldY);
-   //console.log(mPreviousAngle,mCurrentAngle,deltaAngle);
-   // took off degrees = deltaAngle*-1; for testing
+    element.rotate(degrees);
+    set1.rotate(degrees, centerX, centerY);
+    mOldX = ev.pageX - drawOffSetX - centerX;
+    mOldY = ev.pageY - drawOffSetY - centerY;
+  });//end mousemove event
 
-   degrees = deltaAngle*-1;
-
-   //console.log("degrees = "+ degrees);
-   degrees = element.transform("rotation") + degrees;
-
-   if(degrees > 360){
-     degrees = degrees % 360;
-   }
-   //console.log(degrees);
-   element.rotate(degrees);
-   set1.rotate(degrees, centerX, centerY);
-   mOldX = ev.pageX - drawOffSetX - centerX;
-   mOldY = ev.pageY - drawOffSetY - centerY;
-});//end mousemove event
-
-$('svg').on('mouseup',function(ev){
-  if (vmousedown === false){ return; }
-
-   vmousedown = false;
-
-});//end mouseup event
+  $('svg').on('mouseup',function(ev){
+    if (vmousedown === false){ return; }
+     vmousedown = false;
+  });//end mouseup event
 
 
-$('svg').on('click', 'g[name="sandbox"] image', function(){
-      if(a==-1){a +=2;}
-      if(a==0){a+=1}
-      element = SVG.get(this.getAttribute('id'))
+  $('svg').on('click', 'g[name="sandbox"] image', function(){
+    if(a==-1){a +=2;}
+    if(a==0){a+=1}
+    element = SVG.get(this.getAttribute('id'))
 
     if(a == 1 || 0 ){
-       // var cx = element.x();
-       // var cy = element.y();
+
       draw.rotate(0,0,0);
       sandboxFurn.rotate(0);
-
-
       set1.rotate(element.transform("rotation"), centerX, centerY);
-      // console.log('***************************');
-      // console.log(element.transform());
-      // set1.each(function(){ console.log(this.transform() )});
-      // console.log('***************************');
+
       var tx = element.transform("x");
       var ty = element.transform("y");
 
@@ -338,29 +317,20 @@ $('svg').on('click', 'g[name="sandbox"] image', function(){
       set1.transform({y: ty});
 
       //element.transform({ rotation: 37.5 })
-       var x = element.x() - padding;
-       var x2 = x + element.width() + padding;
-       var y = element.y() - padding;
-       var y2 = y + element.height() + padding;
-        selectTop.plot(x, y, x2, y ).stroke({ width: 1});
-        selectBottom.plot(x, y2, x2, y2 ).stroke({ width: 1});
-        selectLeft.plot(x, y, x, y2 ).stroke({ width: 1});
-        selectRight.plot(x2, y, x2, y2 ).stroke({ width: 1});
-        knob.radius(4); knob.move(x-2 +(x2-x)/2,y2+18)
-        connectKnob.radius(4); connectKnob.move(x-2 +(x2-x)/2,y2);
-        connectLine.plot(x+2 +(x2-x)/2,y2+4,x+2 +(x2-x)/2,y2+17).stroke({ width: 1});
-        // draw.rotate(0);
-        // sandboxFurn.rotate(0)
-
-        //console.log(cx,cy);
-        //set1.rotate(0)
-        // set1.rotate(element.transform("rotation"), centerX, centerY)
-
-
-     }
+      var x = element.x() - padding;
+      var x2 = x + element.width() + padding;
+      var y = element.y() - padding;
+      var y2 = y + element.height() + padding;
+      selectTop.plot(x, y, x2, y ).stroke({ width: 1});
+      selectBottom.plot(x, y2, x2, y2 ).stroke({ width: 1});
+      selectLeft.plot(x, y, x, y2 ).stroke({ width: 1});
+      selectRight.plot(x2, y, x2, y2 ).stroke({ width: 1});
+      knob.radius(4); knob.move(x-2 +(x2-x)/2,y2+18)
+      connectKnob.radius(4); connectKnob.move(x-2 +(x2-x)/2,y2);
+      connectLine.plot(x+2 +(x2-x)/2,y2+4,x+2 +(x2-x)/2,y2+17).stroke({ width: 1});
+    }
   })
-
-  })
+})
 
 
 var angleGetter = function (x,y,degree){
@@ -383,4 +353,4 @@ var angleGetter = function (x,y,degree){
     return (degree + 360);
   }
 }
-})
+
